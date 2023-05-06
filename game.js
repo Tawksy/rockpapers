439d8fb9b55a82705179fc05527b069049a88a0e
Tawksy <99350169+Tawksy@users.noreply.github.com>
2023-05-06 18:04:52 +0200

    Update game.js
    Fixxed some issues with the getComputerChoice function
    Rewrote the game function to play the round properly and return the value instead of simply printing it to the console
    Fixxed an issue with the undefined return value from the game

--- game.js
+++ game.js
@@ -11,24 +11,76 @@ let playerChoice = prompt("Please enter which hand you want to play");
 
 if (playableHands.includes(playerChoice.toLowerCase())) {
     let capitalizedHands = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
-    console.log("Valid Choice:", capitalizedHands);
 } else {
     console.log("Invalid Choice!");
     }
   
 
 //-------------------------------------------------
+// Playable hands 
+
+const choices = ["rock", "paper", "scissors"];
 
 
 // Function to get computers Choice, randomly. 
 
 function getComputerChoice() {   
-    const choices = ["Rock", "Paper", "Scissors"];
-    const computerChoice = Math.floor(Math.random() * 3);
-    return choices[computerChoice];
+    
+    const computerChoiceIndex = Math.floor(Math.random() * 3);
+    const computerChoice = choices[computerChoiceIndex];
+    return computerChoice;
     }
 
+let computerHand = getComputerChoice();
+
+
 //----------------------------------------------
+
+//Function to determine ethe outcome of the game 
+
+function outcomeGame(result) {
+    return result;
+}
+// Make computerHand play versus playerChoice 
+
+
+function playTheGame(playerChoice, computerHand) {
+
+
+    if (playerChoice === "rock") {
+        if (computerHand === "rock") {
+            return outcomeGame('Even'); 
+        } else if (computerHand === "paper") {
+            return outcomeGame('You lost!'); 
+        } else {
+            return outcomeGame('You win');
+    }
+}   
+    else if (playerChoice === 'paper') {
+        if (computerHand === 'rock') {
+            return outcomeGame('You win');
+    }   else if (computerHand === 'paper') 
+            return outcomeGame('Even'); 
+       else {
+            return outcomeGame('You lost!');
+}
+}
+    else if (playerChoice === 'scissors') {
+        if (computerHand === 'rock') {
+            return outcomeGame('You lost!');
+    }   else if (computerHand === 'paper') {
+            return outcomeGame('You win');
+    }   else {
+            return outcomeGame('Even');
+    }
+}
+
+}
+
+
+
+console.log(playTheGame(playerChoice, computerHand));
+
 
 
 
